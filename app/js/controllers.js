610eff3b88bc165ab4022e6e7a4ea7324e89235d
julien.girardot <girardot.jul@gmail.com>
2013-12-14 12:31:26 +0100

    maquette html

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,33 +3,33 @@
 /* Controllers */
 
 //angular.module('gameManager.controllers', []).
-    gameManager.controller('ConsoleController', function($scope, $http) {
-        $scope.consoles = [
-            {'name': 'ps1',
-                'games': [{'title':'game1'}, {'title':'game2'}]
-            },
-            {'name': 'ps2',
-                'games': [{'title':'game3'}, {'title':'game4'}]
-            },
-            {'name': 'ps3',
-                'games': [{'title':'game5'}, {'title':'game6'}]
-            }
-        ];
-        /* $http.get('consoles/consoles.json').success(function(data) {
-         $scope.consoles = data;
-         });*/
-  }).
-    controller('ConsoleController2', function($scope) {
+gameManager.controller('GameConsoleController', function ($scope, $http) {
     $scope.consoles = [
         {'name': 'ps1',
-            'games': [{'title':'game1'}, {'title':'game2'}]
+            'games': [
+                {'title': 'game1'},
+                {'title': 'game2'}
+            ]
         },
         {'name': 'ps2',
-            'games': [{'title':'game3'}, {'title':'game4'}]
+            'games': [
+                {'title': 'game3'},
+                {'title': 'game4'}
+            ]
         },
         {'name': 'ps3',
-            'games': [{'title':'game5'}, {'title':'game6'}]
+            'games': [
+                {'title': 'game5'},
+                {'title': 'game6'}
+            ]
         }
     ];
+    $scope.games = [
+        {'title': 'game1'},
+        {'title': 'game2'}
+    ];
+    /* $http.get('consoles/consoles.json').success(function(data) {
+     $scope.consoles = data;
+     });*/
 })
 ;
