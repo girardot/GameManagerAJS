1b58a48f96211629585c4d2004b3a8f93362ebc8
julien.girardot <girardot.jul@gmail.com>
2013-12-14 11:48:27 +0100

    add consoles

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,13 +1,12 @@
-'use strict';
+//'use strict';
 
 /* Controllers */
 
-angular.module('gameManager.controllers', []).
-  controller('ConsoleController', [function() {
+//angular.module('gameManager.controllers', []).
+    gameManager.controller('ConsoleController', function($scope) {
         $scope.consoles = [
             {'name': 'ps1',
                 'games': [{'title':'game1'}, {'title':'game2'}]
-
             },
             {'name': 'ps2',
                 'games': [{'title':'game3'}, {'title':'game4'}]
@@ -16,8 +15,18 @@ angular.module('gameManager.controllers', []).
                 'games': [{'title':'game5'}, {'title':'game6'}]
             }
         ];
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+  }).
+    controller('ConsoleController2', function($scope) {
+    $scope.consoles = [
+        {'name': 'ps1',
+            'games': [{'title':'game1'}, {'title':'game2'}]
+        },
+        {'name': 'ps2',
+            'games': [{'title':'game3'}, {'title':'game4'}]
+        },
+        {'name': 'ps3',
+            'games': [{'title':'game5'}, {'title':'game6'}]
+        }
+    ];
+})
+;
