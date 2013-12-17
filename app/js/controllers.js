01e66fdc8845be64851a2d85e171f9b6f485e205
girardot <girardot@users.noreply.github.com>
2013-12-17 13:39:58 +0100

    add console and games

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,10 +7,22 @@ angular.module('gameManager.controllers', []).
         $http.get('consoles/consoles.json').success(function (data) {
             $scope.consoles = data;
         });
+
+        $scope.addConsole = function() {
+            alert('call web service to add a new console : ' + $scope.newConsole);
+            $scope.consoles.push({"id":4, "name":$scope.newConsole});
+        }
+
     }).
     controller('GameController', function ($scope, $http, $routeParams) {
         $http.get("consoles/"+$routeParams.consoleId+"Games.json").success(function (data) {
             $scope.games = data;
         });
+
+        $scope.addGame = function() {
+            alert('call web service to add a new game : ' + $scope.newGame);
+            $scope.games.push({"title":$scope.newGame});
+        }
+
     })
 ;
