5d527b36631cdd2ead2bee00c990c08b786ff847
girardot <girardot@users.noreply.github.com>
2014-01-03 14:01:11 +0100

    Update Game.java

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,9 +1,19 @@
 package model;
 
-import javax.persistence.*;
+import static model.GameProgression.TO_DO;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import java.io.Serializable;
 
-import static model.GameProgression.TO_DO;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "GAME")
@@ -58,6 +68,7 @@ public class Game implements Serializable {
         this.progression = progression;
     }
 
+    @JsonIgnore
     public Console getConsole() {
         return console;
     }
