3b9fb42db352d6fbe15290331264e933f28a2a35
girardot <girardot@users.noreply.github.com>
2014-01-03 13:56:52 +0100

    Create AbstractIntegrationTest.java

---
+++ src/test/java/repository/AbstractIntegrationTest.java
@@ -0,0 +1,18 @@
+package repository;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
+public class AbstractIntegrationTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+    @Before
+    public void setUp() throws Exception {
+        executeSqlScript("insert_data.sql", false);
+    }
+
+}
