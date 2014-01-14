484c9141fb40b75a918ad85e154cf461bb9fc1f9
girardot <girardot@users.noreply.github.com>
2014-01-14 12:45:17 +0100

    Update controllers.js

--- src/main/resources/app/js/controllers.js
+++ src/main/resources/app/js/controllers.js
@@ -7,14 +7,14 @@ angular.module('gameManager.controllers', []).
 
         $scope.consoles = consoleResource.query();
 
-        $scope.addConsole = function() {
-            consoleResource.save($scope.newConsole, function(data) {
+        $scope.addConsole = function () {
+            consoleResource.save($scope.newConsole, function (data) {
                 $scope.consoles.push(data);
             });
         }
 
-        $scope.removeConsole = function(consoleId) {
-            consoleResource.remove({consoleId: consoleId}, function(data) {
+        $scope.removeConsole = function (consoleId) {
+            consoleResource.remove({consoleId: consoleId}, function (data) {
                 alert($scope.consoles.indexOf(data));
                 $scope.consoles.splice($scope.consoles.indexOf(data), 1);
             });
@@ -23,14 +23,14 @@ angular.module('gameManager.controllers', []).
     controller('GameController', function ($scope, gameResource, $routeParams) {
         $scope.games = gameResource.query({consoleId: $routeParams.consoleId});
 
-        $scope.addGame = function() {
-            gameResource.save($scope.newGame, function(data) {
+        $scope.addGame = function () {
+            gameResource.save({consoleId: $routeParams.consoleId, title: $scope.newGame}, function (data) {
                 $scope.games.push(data);
             });
         }
 
-        $scope.removeGame = function(gameId) {
-            gameResource.remove({gameId: gameId}, function(data) {
+        $scope.removeGame = function (gameId) {
+            gameResource.remove({gameId: gameId}, function (data) {
                 $scope.games.splice($scope.games.indexOf(data), 1);
             });
         }
