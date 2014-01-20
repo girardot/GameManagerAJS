26b4fd1b6a2a90f7cf03472280164b4c1a87f86c
girardot <girardot@users.noreply.github.com>
2014-01-20 09:08:40 +0100

    add JsonSerializer

--- src/main/java/service/JsonConverter.java
+++ src/main/java/service/JsonConverter.java
@@ -1,17 +1,31 @@
 package service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import model.Console;
 import model.Game;
 import org.springframework.stereotype.Service;
+import repository.ConsoleRepository;
 
+import javax.inject.Inject;
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.List;
 
 @Service
 public class JsonConverter {
 
-    final ObjectMapper objectMapper = new ObjectMapper();
+    ConsoleRepository consoleRepository;
+
+    final ObjectMapper objectMapper;
+
+    @Inject
+    public JsonConverter(ConsoleRepository consoleRepository) {
+        objectMapper = new ObjectMapper();
+        objectMapper.configure(JsonParser.Feature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER, true);
+        this.consoleRepository = consoleRepository;
+        objectMapper.registerModule(initModule());
+    }
 
     public String convertToJson(Object toConvert) {
         StringWriter stringWriter = new StringWriter();
@@ -35,6 +49,56 @@ public class JsonConverter {
         }
 
         return game;
+    }
+
+    private final JsonDeserializer<Game> GAME_JSON_DESERIALIZER = new JsonDeserializer<Game>() {
+
+        @Override
+        public Game deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            ObjectCodec oc = jsonParser.getCodec();
+            JsonNode node = oc.readTree(jsonParser);
+            jsonParser.getValueAsString();
+
+            Game game = new Game(node.get("title").toString());
+            Console console = consoleRepository.findById(Long.parseLong(node.get("console_id").toString()));
+            game.setConsole(console);
+
+            return game;
+        }
+    };
+
+    private final JsonSerializer<Game> GAME_JSON_SERIALIZER = new JsonSerializer<Game>() {
+
+
+        public Game deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            ObjectCodec oc = jsonParser.getCodec();
+            JsonNode node = oc.readTree(jsonParser);
+            jsonParser.getValueAsString();
+
+            Game game = new Game(node.get("title").toString());
+            Console console = consoleRepository.findById(Long.parseLong(node.get("console_id").toString()));
+            game.setConsole(console);
+
+            return game;
+        }
+
+        @Override
+        public void serialize(Game game, JsonGenerator jsonGenerator, SerializerProvider provider) throws IOException, JsonProcessingException {
+
+            jsonGenerator.writeStartObject();
+            jsonGenerator.writeNumberField("id", game.getId());
+            jsonGenerator.writeStringField("title", String.valueOf(game.getTitle()));
+            jsonGenerator.writeNumberField("console_id", game.getConsole().getId());
+
+            jsonGenerator.writeEndObject();
+        }
+    };
+
+    private Module initModule() {
+        SimpleModule module = new SimpleModule("GameManagerModul", new Version(1, 0, 0, null, null, null));
+        module.addDeserializer(Game.class, GAME_JSON_DESERIALIZER);
+        module.addSerializer(Game.class, GAME_JSON_SERIALIZER);
+        return module;
     }
 
 }
