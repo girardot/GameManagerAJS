d54481ef35c5d36496e9cfd374f9a4172d781e49
julien <girardot.jul@gmail.com>
2014-04-01 13:30:32 +0200

    test in progress

--- src/main/resources/test/unit/controllersSpec.js
+++ src/main/resources/test/unit/controllersSpec.js
@@ -5,26 +5,32 @@
 describe('controllers', function () {
     beforeEach(module('gameManager.controllers'));
 
+    beforeEach(module('gameManager'));
+
     describe('ConsoleController', function () {
 
-        var scope, consoleController, $httpBackend, consoleResource;
+        var scope, consoleController, $httpBackend;
+
 
         beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
             $httpBackend = _$httpBackend_;
-            $httpBackend.expectGET('/services/console').
+
+            $httpBackend.expectGET('/services/console/').
                 respond([{name: 'ps2'}, {name: 'ps3'}]);
 
             scope = $rootScope.$new();
 
-            consoleController = $controller("ConsoleController", {$scope: scope}, {$resource: consoleResource});
+            consoleController = $controller("ConsoleController", {$scope: scope});
         }));
 
         it('should add a new console', inject(function () {
+
+            console.log(scope.consoles);
             expect(scope.consoles).toEqual([]);
             // when
 
             // then
-            expect(scope.consoles).toEqual([]);
+//            expect(scope.consoles).toEqual([]);
         }));
 
     });
