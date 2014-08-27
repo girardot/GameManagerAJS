299b02771c996972f7c6306484e593150360203a
julien.girardot <girardot.jul@gmail.com>
2014-08-27 21:12:05 +0200

    revert run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 mvn package
 export PORT=5000
-java -Dspring.profiles.active="dev" -cp "target\classes\jgt;target\dependency\*" ServerGameManager
+java -Dspring.profiles.active="dev" -cp "target\classes;target\dependency\*" ServerGameManager
