1f0ffc34f6657467ab37c262329c0114764e3001
julien.girardot <girardot.jul@gmail.com>
2014-09-02 21:31:37 +0200

    try to use sequence defaulting

--- src/main/resources/init-data.sql
+++ src/main/resources/init-data.sql
@@ -1,7 +1,5 @@
-SELECT setval('console_sequence',1);
-INSERT INTO CONSOLE(ID, CONSOLE_NAME) VALUES (nextval('console_sequence'), 'PS3');
+INSERT INTO CONSOLE(ID, CONSOLE_NAME) VALUES (nextval('console_idauto_seq'), 'PS3');
 
-SELECT setval('game_sequence',1);
-INSERT INTO GAME(ID, TITLE, PROGRESSION, CONSOLE_ID) VALUES (nextval('game_sequence'), 'uncharted drake s fortune', 2, currval('console_sequence'));
-INSERT INTO GAME(ID, TITLE, PROGRESSION, CONSOLE_ID) VALUES (nextval('game_sequence'), 'battlefield3 Limited edition', 2, currval('console_sequence'));
-INSERT INTO GAME(ID, TITLE, PROGRESSION, CONSOLE_ID) VALUES (nextval('game_sequence'), 'call of duty Modern warfare 2', 2, currval('console_sequence'));
+INSERT INTO GAME(ID, TITLE, PROGRESSION, CONSOLE_ID) VALUES (nextval('game_idauto_seq'), 'uncharted drake s fortune', 2, currval('console_idauto_seq'));
+INSERT INTO GAME(ID, TITLE, PROGRESSION, CONSOLE_ID) VALUES (nextval('game_idauto_seq'), 'battlefield3 Limited edition', 2, currval('console_idauto_seq'));
+INSERT INTO GAME(ID, TITLE, PROGRESSION, CONSOLE_ID) VALUES (nextval('game_idauto_seq'), 'call of duty Modern warfare 2', 2, currval('console_idauto_seq'));
