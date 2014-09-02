89b63420a7367d6f033613d963e34bfb98e97786
julien.girardot <girardot.jul@gmail.com>
2014-09-02 21:28:39 +0200

    try to init sequence

--- src/main/resources/init-data.sql
+++ src/main/resources/init-data.sql
@@ -1,5 +1,7 @@
+SELECT setval('console_sequence',1);
 INSERT INTO CONSOLE(ID, CONSOLE_NAME) VALUES (nextval('console_sequence'), 'PS3');
 
+SELECT setval('game_sequence',1);
 INSERT INTO GAME(ID, TITLE, PROGRESSION, CONSOLE_ID) VALUES (nextval('game_sequence'), 'uncharted drake s fortune', 2, currval('console_sequence'));
 INSERT INTO GAME(ID, TITLE, PROGRESSION, CONSOLE_ID) VALUES (nextval('game_sequence'), 'battlefield3 Limited edition', 2, currval('console_sequence'));
 INSERT INTO GAME(ID, TITLE, PROGRESSION, CONSOLE_ID) VALUES (nextval('game_sequence'), 'call of duty Modern warfare 2', 2, currval('console_sequence'));
