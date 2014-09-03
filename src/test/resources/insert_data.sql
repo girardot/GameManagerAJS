77e33cde380937749a47ee47f606ec76c68d7e44
julien.girardot <girardot.jul@gmail.com>
2014-09-03 21:39:26 +0200

    correct tests

--- src/test/resources/insert_data.sql
+++ src/test/resources/insert_data.sql
@@ -1,7 +1,7 @@
 TRUNCATE TABLE CONSOLE;
 TRUNCATE TABLE GAME;
-INSERT INTO CONSOLE (id, console_name) VALUES (1, 'ps2');
-INSERT INTO CONSOLE (id, console_name) VALUES (2, 'ps3');
-INSERT INTO GAME (id, title, console_id) VALUES (1, 'call of duty', 2);
-INSERT INTO GAME (id, title, console_id) VALUES (2, 'gta3', 1);
-INSERT INTO GAME (id, title, console_id) VALUES (3, 'nhl 2004', 1);
+INSERT INTO CONSOLE (id, console_name) VALUES (-1, 'ps2');
+INSERT INTO CONSOLE (id, console_name) VALUES (-2, 'ps3');
+INSERT INTO GAME (id, title, console_id) VALUES (-3, 'call of duty', (SELECT id FROM CONSOLE WHERE CONSOLE_NAME='ps3'));
+INSERT INTO GAME (id, title, console_id) VALUES (-4, 'gta3', (SELECT id FROM CONSOLE WHERE CONSOLE_NAME='ps2'));
+INSERT INTO GAME (id, title, console_id) VALUES (-5, 'nhl 2004', (SELECT id FROM CONSOLE WHERE CONSOLE_NAME='ps2'));
