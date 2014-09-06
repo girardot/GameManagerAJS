a569db03384528d5df8b9a0dc885861af4520b3c
julien.girardot <girardot.jul@gmail.com>
2014-09-06 16:56:16 +0200

    revert

--- src/test/java/jgt/repository/GameServiceIntegrationTest.java
+++ src/test/java/jgt/repository/GameServiceIntegrationTest.java
@@ -6,7 +6,7 @@ import jgt.model.Game;
 import org.fest.assertions.api.Assertions;
 import org.junit.Test;
 
-public class GameRepositoryIntegrationTest extends AbstractIntegrationTest {
+public class GameServiceIntegrationTest extends AbstractIntegrationTest {
 
     @Inject
     private GameRepository gameRepository;
