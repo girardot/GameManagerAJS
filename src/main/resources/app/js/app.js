18e3605e3ba2efa6ce536dcedfc3bf2a02e72690
julien.girardot <girardot.jul@gmail.com>
2014-09-13 15:21:15 +0200

    formate

--- src/main/resources/app/js/app.js
+++ src/main/resources/app/js/app.js
@@ -1,22 +1,20 @@
 'use strict';
-//var gameManager = angular.module('gameManager', []);
 
 //Declare app level module which depends on filters, and services
 var gameManager = angular.module('gameManager', [
-  'ngRoute',
-  'ngResource',
-  'gameManager.gameResource',
-  'gameManager.gameStatusResource',
-  'gameManager.gameDematerializeResource',
-  'gameManager.consoleResource',
-  'gameManager.filters',
-  'gameManager.services',
-  'gameManager.directives',
-  'gameManager.controllers'
-])
-    .
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/games/:consoleId', {templateUrl: 'partials/games.html', controller: 'GameController'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+        'ngRoute',
+        'ngResource',
+        'gameManager.gameResource',
+        'gameManager.gameStatusResource',
+        'gameManager.gameDematerializeResource',
+        'gameManager.consoleResource',
+        'gameManager.filters',
+        'gameManager.services',
+        'gameManager.directives',
+        'gameManager.controllers'
+    ])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/games/:consoleId', {templateUrl: 'partials/games.html', controller: 'GameController'});
+        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }]);
