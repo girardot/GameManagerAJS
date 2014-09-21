1309400cf0f83f27fd48ebf5ec0cab81bb46e657
julien.girardot <girardot.jul@gmail.com>
2014-09-21 19:44:09 +0200

    add save method on controller

--- src/main/resources/app/js/controller/gameToBuyListController.js
+++ src/main/resources/app/js/controller/gameToBuyListController.js
@@ -3,5 +3,14 @@
 angular.module('gameManager.gameToBuyControllers', [])
     .controller('GameToBuyListController', function ($scope, gameToBuyResource) {
         $scope.gameToBuyList = gameToBuyResource.query();
+
+        $scope.addGameToBuy = function () {
+
+            gameToBuyResource.save($scope.newGameToBuy, function (data) {
+                $scope.gameToBuyList.push(data);
+                $scope.newGameToBuy = "";
+            });
+        };
+
     })
 ;
