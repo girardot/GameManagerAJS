89e3cdd08f14c8092e45abe34c4164656875f2a8
julien.girardot <girardot.jul@gmail.com>
2014-09-29 20:36:34 +0200

    create to-buy-games directives

--- src/main/resources/app/js/directive/directives.js
+++ src/main/resources/app/js/directive/directives.js
@@ -3,16 +3,27 @@
 angular.module('gameManager.directives', [])
     .directive('deleteModal', function () {
         return {
+            restrict: 'E',
             templateUrl: 'partials/delete-modal.html'
         };
     })
     .directive('consoles', function () {
         return {
+            restrict: 'E',
             templateUrl: 'partials/consoles.html'
         };
     })
     .directive('games', function () {
         return {
+            restrict: 'E',
             templateUrl: 'partials/games.html'
         };
-    });
+    })
+    .directive('toBuyGames', function () {
+
+        return {
+            restrict: 'E',
+            templateUrl: 'partials/tobuyGames.html'
+        };
+    })
+    ;
