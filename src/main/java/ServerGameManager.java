2afaba4f1928de1c874c06a65d18b5f13bd553e4
julien.girardot <girardot.jul@gmail.com>
2014-11-22 19:26:46 +0100

    authentication rest service

--- src/main/java/ServerGameManager.java
+++ src/main/java/ServerGameManager.java
@@ -1,14 +1,15 @@
+import jgt.model.Credentials;
 import jgt.model.GameProgression;
-import jgt.service.ConsoleGameService;
-import jgt.service.JsonConverter;
-import jgt.service.GameToBuyService;
-import jgt.service.JsonTransformer;
+import jgt.service.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import spark.Request;
 
 import static java.lang.Long.parseLong;
+import static org.eclipse.jetty.http.HttpStatus.ACCEPTED_202;
+import static org.eclipse.jetty.http.HttpStatus.UNAUTHORIZED_401;
 import static spark.Spark.*;
 
 public class ServerGameManager {
@@ -19,12 +20,15 @@ public class ServerGameManager {
         Logger logger = LoggerFactory.getLogger(ServerGameManager.class);
 
 
+        final String SESSION_AUTHENTICATION_FIELD = "authenticated";
+
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         final JsonTransformer jsonTransformer = applicationContext.getBean(JsonTransformer.class);
         final JsonConverter jsonConverter = applicationContext.getBean(JsonConverter.class);
         final ConsoleGameService consoleGameService = applicationContext.getBean(ConsoleGameService.class);
         final GameToBuyService toBuyGameService = applicationContext.getBean(GameToBuyService.class);
+        final AuthenticationService authenticationService = applicationContext.getBean(AuthenticationService.class);
 
         staticFileLocation("/app"); // Static files
         setPort(Integer.valueOf(System.getenv("PORT")));
@@ -51,16 +55,45 @@ public class ServerGameManager {
         delete("/services/toBuyGame/:gameToBuyId", (request, response) -> toBuyGameService.deleteGame(parseLong(request.params("gameToBuyId"))));
         post("/services/toBuyGameOrder", (request, response) -> toBuyGameService.changerOrders(request.body()));
 
+        // AUTHENTICATION
+        post("/services/authentication", (request, response) -> {
+
+            Credentials credentials = jsonConverter.convertJsonToCredential(request.body());
+
+            Boolean isAuthenticatedSessionField = request.session().attribute(SESSION_AUTHENTICATION_FIELD) == null ? false : request.session().attribute(SESSION_AUTHENTICATION_FIELD);
+            if (!authenticationService.tryToAuthenticate(credentials, isAuthenticatedSessionField)) {
+                halt(UNAUTHORIZED_401, "Authentication Failed");
+            }
+
+            createSessionIfDoesNotExist(request, credentials.getEmail());
+            response.status(ACCEPTED_202);
+
+            return true;
+        }, jsonTransformer);
 
         // OTHER
         exception(Exception.class, (e, request, response) -> {
             System.out.println(request.body());
             System.out.println(e.getMessage());
+            System.out.println(e);
             response.status(500);
         });
 
-        before((request, response) -> logger.info("request {}", request.pathInfo()));
+        before((request, response) -> {
+            logger.info("request {}", request.pathInfo());
+            if(request.pathInfo().contains("/services/") && request.session().attribute(SESSION_AUTHENTICATION_FIELD) != Boolean.TRUE) {
+                response.redirect("/#/signIn", 401);
+//                halt(401, "You are not welcome here");
+            }
+        });
 
+    }
+
+    private static void createSessionIfDoesNotExist(Request request, String login) {
+        if (request.session().id() == null) {
+            request.session(true);
+            request.session().attribute("email", login);
+        }
     }
 
 }
