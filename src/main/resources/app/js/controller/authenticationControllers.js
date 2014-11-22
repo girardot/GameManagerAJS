d88479fd21069c3713d01a4f34e74a368dbd65aa
julien.girardot <girardot.jul@gmail.com>
2014-11-22 19:11:22 +0100

    AuthenticationController js side

--- src/main/resources/app/js/controller/authenticationControllers.js
+++ src/main/resources/app/js/controller/authenticationControllers.js
@@ -1,11 +1,19 @@
 'use strict';
 
 angular.module('gameManager.authenticationControllers', [])
-    .controller('AuthenticationController', function ($scope) {
+    .controller('AuthenticationController', function ($scope, $location, $http) {
 
-        $scope.login = function() {
-            alert('login with ' + $scope.email + 'password' + $scope.password) ;
+        $scope.login = function (credentials) {
+
+            $http.post('/services/authentication', {'email': credentials.email, 'password': credentials.password})
+                .success(function (user) {
+//                    Auth.setUser(user);
+                    $location.url('/');
+                })
+                .error(function (data) {
+                    $scope.loginError = data.loginError;
+                    $location.url('/signIn');
+                });
         }
-
     })
 ;
