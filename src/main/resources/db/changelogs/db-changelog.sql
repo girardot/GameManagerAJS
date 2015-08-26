a9ee0f506db3d1b0cead3a538eab1e16f1cc9421
julien <girardot.jul@gmail.com>
2015-08-26 20:41:22 +0530

    create user with hibernate

--- src/main/resources/db/changelogs/db-changelog.sql
+++ src/main/resources/db/changelogs/db-changelog.sql
@@ -1,10 +1,3 @@
 --liquibase formatted sql
 
---changeset jgt:1
-create table USER (
-    id bigint not null,
-    email varchar(255),
-    firstname varchar(255),
-    lastname varchar(255),
-    CONSTRAINT PK_USER PRIMARY KEY(ID)
-);
+
