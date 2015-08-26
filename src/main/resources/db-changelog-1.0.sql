d1e9bc3a9375e5756b35884c0d3a108bc6fc43b2
julien <girardot.jul@gmail.com>
2015-08-26 22:01:57 +0530

    test4

--- src/main/resources/db-changelog-1.0.sql
+++ src/main/resources/db-changelog-1.0.sql
@@ -3,14 +3,8 @@
 --changeset jgt:1
 --comment: create user table
 CREATE TABLE USER (
-  ID INT PRIMARY  KEY NOT NULL,
-  EMAIL VARCHAR(255),
-  FIRSTNAME VARCHAR(255),
-  LASTNAME VARCHAR(255)
+  ID INT PRIMARY KEY NOT NULL,
+  EMAIL CHAR(255),
+  FIRSTNAME CHAR(255),
+  LASTNAME CHAR(255)
 );
-
---changeset jgt:2
-alter table CONSOLE add constraint FK_CONSOLE_USER foreign key (user_id) references USER;
-
---changeset jgt:3
-alter table GAME_TO_BUY add constraint FK_GAME_TO_BUY_USER foreign key (user_id) references USER;
