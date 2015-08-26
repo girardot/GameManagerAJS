43c6c2e608aa12709d5015989fd4ce76336b0054
julien <girardot.jul@gmail.com>
2015-08-26 21:45:05 +0530

    test

--- src/main/resources/db-changelog-1.0.sql
+++ src/main/resources/db-changelog-1.0.sql
@@ -1,22 +1,10 @@
 --liquibase formatted sql
 
 --changeset jgt:1
-CREATE TABLE USER (
-    ID bigint NOT NULL,
-    EMAIL VARCHAR(255),
-    FIRSTNAME VARCHAR(255),
-    LASTNAME VARCHAR(255),
-    CONSTRAINT PK_USER PRIMARY KEY (id)
-);
+CREATE TABLE USER (ID bigint NOT NULL,EMAIL VARCHAR(255),FIRSTNAME VARCHAR(255),LASTNAME VARCHAR(255),CONSTRAINT PK_USER PRIMARY KEY (id));
 
 --changeset jgt:2
-alter table CONSOLE
-add constraint FK_CONSOLE_USER
-foreign key (user_id)
-references USER;
+alter table CONSOLE add constraint FK_CONSOLE_USER foreign key (user_id) references USER;
 
 --changeset jgt:3
-alter table GAME_TO_BUY
-add constraint FK_GAME_TO_BUY_USER
-foreign key (user_id)
-references USER;
+alter table GAME_TO_BUY add constraint FK_GAME_TO_BUY_USER foreign key (user_id) references USER;
