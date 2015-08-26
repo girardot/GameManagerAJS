24a209cde5dc160c14a9ad9348d583d17227ebf5
julien <girardot.jul@gmail.com>
2015-08-26 22:16:49 +0530

    update game to buy

--- src/main/resources/db-changelog-1.0.sql
+++ src/main/resources/db-changelog-1.0.sql
@@ -4,3 +4,8 @@
 --comment: create user table
 CREATE TABLE "USER" ( ID INT PRIMARY KEY NOT NULL, EMAIL CHAR(255), FIRSTNAME CHAR(255), LASTNAME CHAR(255));
 
+--changeset jgt:2
+--comment: Update game to buy
+update game_to_buy set user_id=1;
+
+
