e919e4d015200f90c234fa892a767197ada49739
julien <girardot.jul@gmail.com>
2015-08-30 18:23:21 +0530

    update change log

--- src/main/resources/db-changelog-1.0.sql
+++ src/main/resources/db-changelog-1.0.sql
@@ -13,14 +13,16 @@ CREATE TABLE user_account (
 --comment: add julien user
 INSERT INTO user_account (ID, EMAIL, FIRSTNAME, LASTNAME) VALUES (1, 'girardot.jul@gmail.com', 'julien', 'girardot');
 
---changeset jgt:2
---comment: add FK
-ALTER TABLE CONSOLE
-ADD CONSTRAINT FK_CONSOLE_USER_ACCOUNT
+--changeset jgt:3
+--comment: add fk_console_user_account
+ALTER TABLE console
+ADD CONSTRAINT fk_console_user_account
 FOREIGN KEY (user_id)
 REFERENCES user_account;
 
-ALTER TABLE GAME_TO_BUY
-ADD CONSTRAINT FK_GAME_TO_BUY_USER_ACCOUNT
+--changeset jgt:4
+--comment: add fk_game_to_buy_user_account
+ALTER TABLE game_to_buy
+ADD CONSTRAINT fk_game_to_buy_user_account
 FOREIGN KEY (user_id)
 REFERENCES user_account;
