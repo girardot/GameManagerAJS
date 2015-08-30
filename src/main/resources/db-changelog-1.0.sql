83323644c14ab52b846aa749748770040b6760ca
julien <girardot.jul@gmail.com>
2015-08-30 18:33:55 +0530

    test

--- src/main/resources/db-changelog-1.0.sql
+++ src/main/resources/db-changelog-1.0.sql
@@ -2,7 +2,7 @@
 
 --changeset jgt:1
 --comment: create user table
-CREATE TABLE "user_account" (
+CREATE TABLE user_account (
   ID        BIGINT PRIMARY KEY NOT NULL,
   EMAIL     VARCHAR(255),
   FIRSTNAME VARCHAR(255),
@@ -11,4 +11,4 @@ CREATE TABLE "user_account" (
 
 --changeset jgt:2
 --comment: add julien user
-INSERT INTO "user_account" (ID, EMAIL, FIRSTNAME, LASTNAME) VALUES (1, 'girardot.jul@gmail.com', 'julien', 'girardot');
+INSERT INTO user_account (ID, EMAIL, FIRSTNAME, LASTNAME) VALUES (1, 'girardot.jul@gmail.com', 'julien', 'girardot');
