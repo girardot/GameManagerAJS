d53c8f2fea87e320fb01748fbefdfe3f59b3e349
julien <girardot.jul@gmail.com>
2015-08-30 17:08:00 +0530

    add julien user

--- src/main/resources/db-changelog-1.0.sql
+++ src/main/resources/db-changelog-1.0.sql
@@ -2,5 +2,13 @@
 
 --changeset jgt:1
 --comment: create user table
-CREATE TABLE "user" ( ID BIGINT PRIMARY KEY NOT NULL, EMAIL VARCHAR(255), FIRSTNAME VARCHAR(255), LASTNAME VARCHAR(255));
+CREATE TABLE "user" (
+  ID        BIGINT PRIMARY KEY NOT NULL,
+  EMAIL     VARCHAR(255),
+  FIRSTNAME VARCHAR(255),
+  LASTNAME  VARCHAR(255)
+);
 
+--changeset jgt:2
+--comment: add julien user
+INSERT INTO "user" (ID, EMAIL, FIRSTNAME, LASTNAME) VALUES (1, 'girardot.jul@gmail.com', 'julien', 'girardot');
