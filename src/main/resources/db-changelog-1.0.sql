3aef442935aa68f2d29fe560976759864332ef8d
julien <girardot.jul@gmail.com>
2015-08-30 18:29:41 +0530

    try withou FK

--- src/main/resources/db-changelog-1.0.sql
+++ src/main/resources/db-changelog-1.0.sql
@@ -12,17 +12,3 @@ CREATE TABLE "user_account" (
 --changeset jgt:2
 --comment: add julien user
 INSERT INTO "user_account" (ID, EMAIL, FIRSTNAME, LASTNAME) VALUES (1, 'girardot.jul@gmail.com', 'julien', 'girardot');
-
---changeset jgt:3
---comment: add fk_console_user_account
-ALTER TABLE "console"
-ADD CONSTRAINT fk_console_user_account
-FOREIGN KEY (user_id)
-REFERENCES "user_account";
-
---changeset jgt:4
---comment: add fk_game_to_buy_user_account
-ALTER TABLE "game_to_buy"
-ADD CONSTRAINT fk_game_to_buy_user_account
-FOREIGN KEY (user_id)
-REFERENCES "user_account";
