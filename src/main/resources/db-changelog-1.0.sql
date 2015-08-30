5784a2003dfd9c9f7147a05063be3a2f24ef3bc6
julien <girardot.jul@gmail.com>
2015-08-30 18:37:00 +0530

    test

--- src/main/resources/db-changelog-1.0.sql
+++ src/main/resources/db-changelog-1.0.sql
@@ -8,7 +8,3 @@ CREATE TABLE user_account (
   FIRSTNAME VARCHAR(255),
   LASTNAME  VARCHAR(255)
 );
-
---changeset jgt:2
---comment: add julien user
-INSERT INTO user_account (ID, EMAIL, FIRSTNAME, LASTNAME) VALUES (1, 'girardot.jul@gmail.com', 'julien', 'girardot');
