ec27901dd30c51a37117e8a200ddfeb1402afbc1
julien <girardot.jul@gmail.com>
2015-08-30 18:27:02 +0530

    update change log2

--- src/main/resources/db-changelog-1.0.sql
+++ src/main/resources/db-changelog-1.0.sql
@@ -2,7 +2,7 @@
 
 --changeset jgt:1
 --comment: create user table
-CREATE TABLE user_account (
+CREATE TABLE "user_account" (
   ID        BIGINT PRIMARY KEY NOT NULL,
   EMAIL     VARCHAR(255),
   FIRSTNAME VARCHAR(255),
@@ -11,18 +11,18 @@ CREATE TABLE user_account (
 
 --changeset jgt:2
 --comment: add julien user
-INSERT INTO user_account (ID, EMAIL, FIRSTNAME, LASTNAME) VALUES (1, 'girardot.jul@gmail.com', 'julien', 'girardot');
+INSERT INTO "user_account" (ID, EMAIL, FIRSTNAME, LASTNAME) VALUES (1, 'girardot.jul@gmail.com', 'julien', 'girardot');
 
 --changeset jgt:3
 --comment: add fk_console_user_account
-ALTER TABLE console
+ALTER TABLE "console"
 ADD CONSTRAINT fk_console_user_account
 FOREIGN KEY (user_id)
-REFERENCES user_account;
+REFERENCES "user_account";
 
 --changeset jgt:4
 --comment: add fk_game_to_buy_user_account
-ALTER TABLE game_to_buy
+ALTER TABLE "game_to_buy"
 ADD CONSTRAINT fk_game_to_buy_user_account
 FOREIGN KEY (user_id)
-REFERENCES user_account;
+REFERENCES "user_account";
